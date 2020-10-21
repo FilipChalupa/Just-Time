4744ae7d2cce2a2266fda3efc0599e278a9d47d9
Filip Chalupa <chalupa.filip@gmail.com>
2020-10-21 15:20:38 +0200

    Icon: add maskable variant

--- icon/resize.sh
+++ icon/resize.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-original="original.svg"
 sizes=(36 48 72 96 128 144 192 256 384 512 1024 2048)
 
 for i in "${sizes[@]}"; do
 	echo Resizing $i
 	name=$i"x"$i".png"
-	inkscape.exe -z $original --export-width $i -e $name
+	inkscape.exe -z "favicon.svg" --export-width $i -e $name
+
+	name=$i"x"$i"-maskable.png"
+	inkscape.exe -z "maskable.svg" --export-width $i -e $name
 done
 
 echo Done
