397f7d611d1d6e512fe988a22ae01c1b41593f52
Filip Chalupa <chalupa.filip@gmail.com>
2021-08-02 21:23:09 +0200

    Resize using rsvg-convert

--- icon/resize.sh
+++ icon/resize.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+directory="$PWD/"
+icon="favicon.svg"
+iconMaskable="maskable.svg"
 sizes=(36 48 72 96 128 144 192 256 384 512 1024 2048)
 
 for i in "${sizes[@]}"; do
 	echo Resizing $i
+
 	name=$i"x"$i".png"
-	inkscape.exe -z "favicon.svg" --export-width $i -e $name
+	rsvg-convert -w $i -h $i "$directory$icon" -o "$directory$name"
 
 	name=$i"x"$i"-maskable.png"
-	inkscape.exe -z "maskable.svg" --export-width $i -e $name
+	rsvg-convert -w $i -h $i "$directory$iconMaskable" -o "$directory$name"
 done
 
 echo Done
